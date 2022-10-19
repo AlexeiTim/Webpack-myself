ceda32c4d5de1786e5b5e88774c60588f387d697
AlexeiTim <timashkov060896@gmail.com>
2022-10-19 14:41:01 +0300

    personal webpack

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { stringify } = require('querystring');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+const { isLiteralTypeNode } = require('typescript');
+
+const esLintPlugin = (isDev) => isDev ? [] : [new ESLintWebpackPlugin({
+  extensions: ['ts', 'js']
+})]
+
+module.exports = (develop) => ({
+  mode: 'development',
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: './assets/[hash][ext][query]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      
+      // {
+      //   test: /\.css$/i,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      // },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: './public' }
+    //   ]
+    // }),
+   ...esLintPlugin(develop)
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    hot: true,
+    open: true,
+    port: 8080,
+    compress: true,
+    historyApiFallback: true
+  },
+})
